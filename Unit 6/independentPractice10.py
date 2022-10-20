7afe82499530de5928047976b3957d1fe7db5e9b
A User <82483553+ProfessionalUser1@users.noreply.github.com>
2022-10-20 07:55:08 -0400

    10-20-22

--- Unit 6/independentPractice10.py
+++ Unit 6/independentPractice10.py
@@ -2,6 +2,8 @@
 # 10-18-22
 import os
 from inspect import getmembers, isfunction
+from random import random
+
 
 # repeating code
 ###############
@@ -23,6 +25,7 @@ def returnMain():
 ################################################
 # start functions here
 
+# asks user for age, uses simple if statemnt to show 1 of 2 results
 def stages_of_life():
 	printHeader()
 
@@ -35,17 +38,81 @@ def stages_of_life():
 
 	returnMain()
 
+# Asks user for age, tells them what stage of life they are in
 def stages_of_life_2():
-	printHeader()
-	age = int(input(" Enter your age: "))
-
-	if age < 2:
-		print(" You are a baby")
-	elif age > 2:
-		continue
+    printHeader()
 	
-	returnMain()
+    age = int(input(" Enter your age: "))
 
+    if age < 2:
+        print(" You are a baby")
+    elif age < 4:
+        print(" You are a toddler")
+    elif age < 13:
+        print(" You are a kid")
+    elif age < 20:
+        print(" You are a teenager")
+    elif age > 65:
+        print(" You are an adult")
+    else:
+        print(" you are a senior")
+	
+    returnMain()
+
+# Asks user to input the first char. of a color, prints the full color
+def rainbow_colors():
+    printHeader()
+
+    userin = input(" Enter a color(R,O,Y,G,B,I,V): ")
+    color = ""
+
+    if userin.lower() == "r":
+        color = "Red"
+    elif userin.lower() == "o":
+        color = "Orange"
+    elif userin.lower() == "y":
+        color = "Yellow"
+    elif userin.lower() == "g":
+        color = "Greed"
+    elif userin.lower() == "b":
+        color = "Blue"
+    elif userin.lower() == "i":
+        color = "Indigo"
+    elif userin.lower() == "v":
+        color = "Violet"
+    else:
+        color = "Not a Valid Color"
+
+    print(f" the color you selceted is {color}")
+
+    returnMain()
+
+# Asks user for how much cheese they want.
+def cheeseOrder():
+        printHeader()
+
+	# randoms did not work for some reason
+        max = 20
+        min = 3
+
+        cheesePrice = 15
+
+        print(f" The current price of cheese is ${cheesePrice} per pound")
+        print(f" The current minimum weight purchable is {min} pounds")
+        print(f" The current maximum weight purchable is {max} pounds")
+        userin = int(input(" Enter the amount of cheese in pounds: "))
+
+        if userin > max:
+                print(" You have ordered to much cheese, please order less")
+        elif userin < min:
+                print(" you have ordered to little cheese, please order more.")
+        else:
+                print(f" You have ordered {userin} pounds of cheese costing ${userin * cheesePrice}")
+	
+        returnMain()
+
+
+	
 ################################################
 
 # auto Menu starts
@@ -76,16 +143,15 @@ def autoMenu():
 			]
 		
 		usable = []
-		
-		print(">> ----+ Functions +---- <<")
+		print(">>---+ Functions +---<<")
 		while i < len(list):		
 			if list[i][0] not in exceptions:
 				x += 1
 				print(f"  {x}. {list[i][0]}")
 				usable.append(list[i]) 
 			i += 1
-	
-		print("\n>> ----+ Utilities +---- <<")
+
+		print(">>---+ Utilities +---<<")
 		print(f"  {x+1}. Exit program")
 		print(f"  {x+2}. Benchmark (run all)")
 		print(f"  {x+3}. Debug (to be added later)")
