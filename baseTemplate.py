65cf876af19ea8aab2941174e331308c712cb164
A User <82483553+ProfessionalUser1@users.noreply.github.com>
2022-10-20 07:54:35 -0400

    Update baseTemplate.py

--- baseTemplate.py
+++ baseTemplate.py
@@ -24,19 +24,36 @@ def returnMain():
 # start functions here
 
 
+
 ################################################
 
-# auto Menu starts
-benchMode = False
+menuMode = 0
 currentBench = 0
 
 def autoMenu():	
-	global benchMode
+	global menuMode
 	global currentBench
 	global usable
 
-	if benchMode != True:
+	if menuMode == 1:
+		if currentBench > len(usable):
+			menuMode = 0
+			currentBench = 0
+			autoMenu()
+		else:
+			currentBench += 1
+			usable[currentBench-1][1]()
 
+	if menuMode == 2:
+		if currentBench > len(usable):
+			menuMode = 0
+			currentBench = 0
+			autoMenu()
+		else:
+			currentBench += 1
+			usable[currentBench-1][1]()
+	
+	else:
 		printHeader()
 
 		# change were it says 'baseTemplate' to module name.
@@ -53,48 +70,39 @@ def autoMenu():
 		
 		usable = []
 		
+		print(">> ----+ Functions +---- <<")
 		while i < len(list):		
 			if list[i][0] not in exceptions:
 				x += 1
 				print(f"  {x}. {list[i][0]}")
 				usable.append(list[i]) 
 			i += 1
-	
-		x += 1
-		print(f"  {x}. Exit program")
-	
-		x += 1
-		print(f"  {x}. Benchmark (run all)")
-	
-		x += 1
-		print(f"  {x}. Debug (to be added later)")
-		
+
+		print("\n>> ----+ Utilities +---- <<")
+		print(f"  {x+1}. Exit program")
+		print(f"  {x+2}. Benchmark (run all)")
+		print(f"  {x+3}. Debug (to be added later)")
 		
 		try:
 			usrin = int(input("\n Enter number of the item: "))
 			if usrin == len(usable) + 1:
 				SystemExit()
 			elif usrin == len(usable) + 2:
-				benchMode = True
+				menuMode = 1
 				print("Bench")
 				autoMenu()
 			elif usrin == len(usable) + 3:
-				SystemExit()
+				menuMode = 2
+				print("Debug")
+				autoMenu()
 			else:
 				usable[usrin-1][1]()
 		
 		except:
 			autoMenu()
 			
-	else:
-		if currentBench > len(usable):
-			benchMode = False
-			currentBench = 0
-			autoMenu()
-		else:
-			currentBench += 1
-			usable[currentBench-1][1]()
-			
 # auto menu ends
-# program start
+
+#program start
 autoMenu()
+
