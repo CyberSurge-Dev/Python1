7585cb91dde006bbc16857fe7da09c9361470f0d
A User <82483553+ProfessionalUser1@users.noreply.github.com>
2022-10-28 08:10:09 -0400

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -1,7 +1,9 @@
 # Zachary Hoover || File Runner v1.0
 # 10-27-22
 
+from asyncio import exceptions
 import os
+from re import X
 
 global clear
 clear = lambda: os.system("cls")
@@ -24,17 +26,41 @@ def main():
     while i < len(folders):
         print(f"  {i+1}. {folders[i]}")
         i += 1
-    print(f"  {i}. Current Dir")
+    print(f"  {i+1}. Current Dir")
 
     userIN = int(input("\n Enter number of the item: "))
-    if (userIN > len(folders)) or (userIN < 1):
-        main()
-    else:
+    if userIN == len(folders) + 1:
         usable = []
         clear()
         print(">> ----+ Files +---- <<")
+
+        x = 1
+        for filename in os.listdir():
+            if filename not in folders:
+                print(f"  {x}. {filename}")
+                usable.append(filename)
+                x += 1
+        
+        print("\n>> ----+ Utils +---- <<")
+        print(f"  {x}. Return to menu")
+
+        userIN = int(input("\n Enter number of the item: "))
+
+        if (userIN > len(usable)) or (userIN < 0):
+            main()
+        else:
+            # f"{selectedFolder}\\{usable[userIN-1]}"
+            os.startfile(f"{usable[userIN-1]}")
+    elif (userIN > len(folders)) or (userIN < 1):
+        main()
+    else:
+        selectedFolder = folders[userIN-1]
+        usable = []
+        clear()
+        print(">> ----+ Files +---- <<")
+        
         i = 1
-        for filename in os.listdir(folders[i-1]):
+        for filename in os.listdir(folders[userIN-1]):
             print(f"  {i}. {filename}")
             usable.append(filename)
             i += 1
@@ -47,5 +73,6 @@ def main():
         if (userIN > len(usable)) or (userIN < 0):
             main()
         else:
-            exec(open(usable[userIN-1]).read())
+            # f"{selectedFolder}\\{usable[userIN-1]}"
+            os.startfile(f"{selectedFolder}\\{usable[userIN-1]}")
 main()
